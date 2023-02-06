744d08f254f4cb80797a2131eb7aacd5b25bff53
Mattias Gaspar <mattiasmac2000@gmail.com>
2023-02-06 15:30:12 -0300

    build add

--- dist/routes/user.js
+++ dist/routes/user.js
@@ -5,12 +5,12 @@ var _loginRequired = require('../middlewares/loginRequired'); var _loginRequired
 
 const router = new (0, _express.Router)();
 
-// nessa aplicação essas rotas não devem existir // falha de segurança
+// nesta aplicação essas rotas não devem existir -> falha de segurança
 // router.get('/', userController.index); // Listar todos os usuários
 // router.get('/', loginRequired, userController.show); // Mostrar um usuário pelo id
 
 // Rotas que devem existir para essa aplicação
-router.post('/', _loginRequired2.default, _User2.default.store); // Criar usuário
+router.post('/', _User2.default.store); // Criar usuário
 router.put('/', _loginRequired2.default, _User2.default.update); // Atualizar dados do usuário
 router.delete('/', _loginRequired2.default, _User2.default.delete); // Deletar usuário
 
